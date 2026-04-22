0263ad79f6cbe86842a1ae035b400767b49324ba
palmbralee2811-gif <palmbralee2811@gmail.com>
2026-04-22 10:19:31 +0700

    Refactor gRPC mock server implementation

--- GRPC/grpc/mock-server.js
+++ GRPC/grpc/mock-server.js
@@ -3,13 +3,13 @@ const protoLoader = require('@grpc/proto-loader');
 const path = require('path');
 
 
-// 1. ล็อกเป้าหมายไฟล์ .proto ให้อยู่โฟลเดอร์เดียวกันเป๊ะๆ
+
 const PROTO_PATH = path.join(__dirname, 'livevoice.proto');
 const packageDefinition = protoLoader.loadSync(PROTO_PATH, {});
 const protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
 const livevoice = protoDescriptor.livevoice;
 
-// 2. สร้างฟังก์ชันจำลองการทำงาน
+
 function translateAudio(call, callback) {
   callback(null, {
     translated_text: "สวัสดี นี่คือข้อความจำลองจาก Server",
@@ -17,11 +17,11 @@ function translateAudio(call, callback) {
   });
 }
 
-// 3. เปิดสวิตช์ Server
+
 const server = new grpc.Server();
 server.addService(livevoice.VoiceService.service, { TranslateAudio: translateAudio });
 
-// สั่งให้ Server ไปรอรับสายที่พอร์ต 50051
+
 server.bindAsync('127.0.0.1:50051', grpc.ServerCredentials.createInsecure(), (err, port) => {
   if (err) {
     console.error("เกิดข้อผิดพลาด:", err);
